8f43f465efc34eb8fd7ff65b9c8c6cbe913b24d9
Alexander Derevyanko <74137399+derevyankoolx@users.noreply.github.com>
2020-12-28 21:10:45 +0300

    complet commit

--- js/main.js
+++ js/main.js
@@ -34,21 +34,6 @@ function clock() {
 }
 clock();
 
-$("form").submit(function() { //Change
-    var th = $(this);
-    $.ajax({
-        type: "POST",
-        url: "mail.php", //Change
-        data: th.serialize()
-    }).done(function() {
-        alert("Thank you!");
-        setTimeout(function() {
-            // Done Functions
-            th.trigger("reset");
-        }, 1000);
-    });
-    return false;
-});
 
 $(document).ready(function () {
     $('.info_content').click(function () {
@@ -58,3 +43,20 @@ $(document).ready(function () {
 });
 
 
+
+$(document).ready(function() {
+
+	$("#form").submit(function() {
+		$.ajax({
+			type: "POST",
+			url: "mail.php",
+			data: $(this).serialize()
+		}).done(function() {
+			$(this).find("input").val("");
+			alert("Спасибо за заявку! Скоро мы с вами свяжемся.");
+			$("#form").trigger("reset");
+		});
+		return false;
+	});
+	
+});
